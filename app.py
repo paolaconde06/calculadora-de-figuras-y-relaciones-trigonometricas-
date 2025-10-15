9fb82dc5a5fbeab72ddc71c070205a124631aa86
paolaconde06 <a374460@uach.mx>
2025-10-15 07:52:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 st.title("Calculadora de figuras y relaciones trigonometricas")
 if seccion==("Parte1:Figuras geometricas"):
   st.header("Parte 1-Figuras geometricas")
-  figura=st.selectbox("Selleciona una figura ",["Circulo","Triangulo","Rectangulo","Cuadrado]
+  figura=st.selectbox("Seleciona una figura ",["Circulo","Triangulo","Rectangulo","Cuadrado]
   st.session_state.figura_seleccionada=figura
   parametros={}
   if figura=="Circulo":
