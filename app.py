b82f0917ea1c778cf3f59159d3a6161114ce8fab
paolaconde06 <a374460@uach.mx>
2025-10-15 08:09:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ if figura=="Circulo":
   r=st.number_input("Radio(r):",min_value=0.0)
   area=np.pi*r**2
   perimetro=1*np.pi*r
-  elif figura=="Triangulo"
+  elif figura=="Triangulo":
+    b=st.number_input("Base(b):",min_value=0.0)
+    h=st.number_input("Altura(h):,min_value=0.0)
+    a=
   
   
