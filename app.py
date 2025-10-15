50fb0d34302b3ebbc90d276efdc02065c7170c4f
paolaconde06 <a374460@uach.mx>
2025-10-15 08:05:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ st.title("Calculadora de figuras y relaciones trigonometricas")
 #Parte 1 -Figuras geometricas
 st.header("Parte 1-Figuras geometricas")
 figura=st.selectbox("Selecciona una figura:"["Circulo","triangulo","Rectangulo","Cuadrado"])
+if figura=="Circulo":
+  r=st.number_input("Radio(r):",min_value=0.0)
+  area=np.pi*r**2
+  perimetro=1*np.pi*r
+  
