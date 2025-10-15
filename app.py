f3d93dd1e03df29850c334d3c7cc46dbe7982dde
paolaconde06 <a374460@uach.mx>
2025-10-15 08:06:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 st.title("Calculadora de figuras y relaciones trigonometricas")
 #Parte 1 -Figuras geometricas
 st.header("Parte 1-Figuras geometricas")
-figura=st.selectbox("Selecciona una figura:"["Circulo","triangulo","Rectangulo","Cuadrado"])
+figura=st.selectbox("Selecciona una figura:",["Circulo","triangulo","Rectangulo","Cuadrado"])
 if figura=="Circulo":
   r=st.number_input("Radio(r):",min_value=0.0)
   area=np.pi*r**2
   perimetro=1*np.pi*r
+  elig figura=="Triangulo"
   
   
