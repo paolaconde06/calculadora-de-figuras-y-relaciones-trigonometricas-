6a0a4851c820dcd15ad2f9d9ff1883eac643a87f
paolaconde06 <a374460@uach.mx>
2025-10-15 08:11:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,5 @@ if figura=="Circulo":
   perimetro=1*np.pi*r
   elif figura=="Triangulo":
     b=st.number_input("Base(b):",min_value=0.0)
-    h=st.number_input("Altura(h):,min_value=0.0)
-    a=
-  
-  
+    h=st.number_input("Altura(h):",min_value=0.0)
+    a=st.number_input("Lado (A):",min_value=0.0)
